f3e413a44f16e1a44087d9aa16dca80a84f8c501
kientran1705 <kien.tlt.1957@aptechlearning.edu.vn>
2026-09-04 23:35:15 +0700

    feat: bao cao license ve MiniSSO khi khoi dong (cong khai, khong chan app)

--- Program.cs
+++ Program.cs
@@ -49,6 +49,7 @@ using (var scope = app.Services.CreateScope())
     await Seeder.SeedAsync(scope.ServiceProvider.GetRequiredService<AppDbContext>());
 
 app.UseFleetObs();
+FleetObs.ReportLicense(ssoAuthority, "ministamp");
 app.UseCors();
 app.UseAuthentication();
 app.UseAuthorization();
